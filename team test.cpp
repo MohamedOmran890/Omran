0597d11d20be9f9d08a4108bbeb4e8cc38436b13
Hazem Abdelsatter <hazemabdelsattar2002@gmail.com>
2023-03-19 20:20:53 +0200

    Create team test.cpp

---
+++ team test.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct str
+{
+    int a,b,c,d,sum = 0;
+    string n;
+};
+
+bool com ( str a, str b )
+{
+    if ( abs(a.sum - b.sum) > 10 )
+        return a.sum > b.sum;
+    else
+        return a.n < b.n;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    //freopen("time.in","r",stdin);
+    int num;
+    cin>>num;
+    cout<<num;
+    return 0;
+}
